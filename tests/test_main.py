2c0dfc7f3f11fb78ba99929bab83a2d9bcbd17d3
AlbinAndersson1337 <118989634+AlbinAndersson1337@users.noreply.github.com>
2024-08-23 15:35:17 +0200

    Social media

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,20 @@
 from seleniumbase import BaseCase
- 
 import pathlib
  
 filePath = "file://" + \
     str(pathlib.Path(__file__).parent.resolve())[:-5].replace("\\", "/")
- 
-startPage = filePath + "index.html"  # Path to index.html
- 
-class testworkingWebsite(BaseCase):
 
+#Filsökvägen till index.htmldfs
+startPage = filePath + "index.html"  
+ 
+#testar huvuddelen av hemsidan
+class testworkingWebsite(BaseCase):
+    #testar att namnet på hemsidan är korrekt
     def test_Title(self):
         self.open(startPage)
         self.assert_text("Florista" , "h1")
         
+    #testar att sidan tillhär företaget visas
     def test_PageMessage(self):
         self.open(startPage)
         self.assert_text("Den här hemsidan tillhör florista" , "p")
