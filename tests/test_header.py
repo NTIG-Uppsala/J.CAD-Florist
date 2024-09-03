5a15940f7be72dd215273d11cbca619dab2bd03d
CarlErikssonSkogh <carl.erikssonskogh@elev.ga.ntig.se>
2024-09-03 14:45:17 +0200

    Optimerade header testen
    Satte ihop 3 tester till ett enda större test

--- tests/test_header.py
+++ tests/test_header.py
@@ -8,17 +8,10 @@ filePath = "file://" + str(pathlib.Path(__file__).parent.resolve())[:-5].replace
 startPage = filePath + "index.html"  
 
 class header_test(BaseCase):
-    # Testar att loggan finns i headern
-    def testLogo(self):
+    #testar att all info finns på headern
+    def testHeaderInfo(self):
         self.open(startPage)
         self.assert_element(".header [src=\"images/logga_florist.png\"]")
-
-    # Kollar om titel finns på sidan
-    def test_Title(self):
-        self.open(startPage)
         self.assert_text("florista")
-        
-    # Kollar om texten "Den här hemsidan tillhör florista" finns på sidan
-    def test_PageMessage(self):
-        self.open(startPage)
         self.assert_text("Den här hemsidan tillhör florista")
+
