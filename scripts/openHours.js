0434f103da552e9ebd3732599c27aedde63de839
CarlErikssonSkogh <carl.erikssonskogh@elev.ga.ntig.se>
2024-09-05 14:58:21 +0200

    Tog bort onödiga rader i oepnHours test och javascript

--- scripts/openHours.js
+++ scripts/openHours.js
@@ -8,7 +8,6 @@ openHoursDict = {
     'söndag': {'open': '12', 'close': '15'}
 }
 
-//Stängda dagar
 closedDays = [
     "1/1",
     "6/1",
@@ -20,8 +19,6 @@ closedDays = [
     "31/12"
 ]
 
-
-
 function getTodaysDate(date) {
     const month = date.getMonth();  // 0-11
     const dayOfMonth = date.getDate();
@@ -29,21 +26,17 @@ function getTodaysDate(date) {
     return todaysDate;
 }
 
-
 function checkOpenHours(date) {
     //räknar ut vilken veckodag det är idag och imorgon
     let day = date.getDay()
-    console.log(day);
-    if (day == 0) {
+    if (day == 0) { // day = 0 är söndag men i openHoursDict är söndag sista dagen (index 6)
         currentWeekDay = Object.keys(openHoursDict)[6];
-    } else {
+    } else { // indexeringen i date.getDay() är 0-6 där 0 är söndag, så vi behöver subtrahera 1 för att få rätt index
         currentWeekDay = Object.keys(openHoursDict)[day - 1];
     }
-    console.log("current",currentWeekDay);
     const nextWeekDay = Object.keys(openHoursDict)[day];
-    console.log("next",nextWeekDay);
-    //Texten som ska stylas
-    const openHoursText = document.getElementById("openOrClosed");
+    
+    const openHoursText = document.getElementById("openOrClosed"); //Texten som ska stylas
 
     const nextDayOpenHour = openHoursDict[nextWeekDay].open
     const currentDayOpenHour = openHoursDict[currentWeekDay].open;
--- tests/test_openHours.py
+++ tests/test_openHours.py
@@ -1,52 +1,11 @@
 from seleniumbase import BaseCase
 import pathlib
-from datetime import datetime
 import logging
 # Configure logging
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 # Filsökvägen till rotmappen
 filePath = "file://" + str(pathlib.Path(__file__).parent.resolve())[:-5].replace("\\", "/")
 
-#Dictionary som översätter veckodagar från engelska till svenska
-english_to_swedish = {
-    'Monday': 'Måndag',
-    'Tuesday': 'Tisdag',
-    'Wednesday': 'Onsdag',
-    'Thursday': 'Torsdag',
-    'Friday': 'Fredag',
-    'Saturday': 'Lördag',
-    'Sunday': 'Söndag'
-}
-
-openHoursDict = {
-    'Måndag': {'open': '10', 'close': '18'},
-    'Tisdag': {'open': '10', 'close': '18'},
-    'Onsdag': {'open': '10', 'close': '17'},
-    'Torsdag': {'open': '10', 'close': '17'},
-    'Fredag': {'open': '10', 'close': '18'},
-    'Lördag': {'open': '12', 'close': '16'},
-    'Söndag': {'open': '12', 'close': '15'}
-}
-closedDays = [
-    "01/01",
-    "06/01",
-    "01/05",
-    "06/06",
-    "24/12",
-    "25/12",
-    "26/12",
-    "31/12"
-]
-#datum och tid just nu 
-now = datetime.now()
-
-#formaterar datum, tid och veckodag separat
-date_string = now.strftime("%d/%m")
-time_string = now.strftime("%H:%M:%S")
-weekday_string_english = now.strftime("%A")
-weekday_string = english_to_swedish[weekday_string_english]
-days = list(openHoursDict.keys())
-nextDay = days[(days.index(weekday_string) + 1)]
 #Filsökväg till index.html
 startPage = filePath + "index.html"
 
