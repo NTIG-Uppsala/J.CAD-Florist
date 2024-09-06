efb9cae7e6ab1618c448ba0d44e0d99c702953a3
JoelJanssson <joel.jansson@elev.ga.ntig.se>
2024-09-06 09:22:53 +0200

    Optimerade data-include scriptet och kommenterade kod
    Tog bort onödig kod i data-include som var kvar från produkt carouselen.

--- scripts/data-include.js
+++ scripts/data-include.js
@@ -1,13 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
+//Script som inkluderar html-filer i andra html-filer
+
+document.addEventListener("DOMContentLoaded", function() { //väntar på att hela sidan laddas in innan scriptet körs
     var includes = document.querySelectorAll('[data-include]');
-    includes.forEach(function(include) {
+    includes.forEach(function(include) { //hämtar filnamn från 'data-include' attributet och lägger till .html
         var file = include.getAttribute('data-include') + '.html';
-        fetch(file)
+        fetch(file) //hämtar html-filen
             .then(response => response.text())
             .then(data => {
-                include.innerHTML = data;
-                const fragment = document.createDocumentFragment();
-                include.appendChild(fragment);
+                include.innerHTML = data; //lägger in html-koden i elementet
             })
             .catch(error => console.error('Error loading file:', error));
     });
--- tests/test_products.py
+++ tests/test_products.py
@@ -3,9 +3,11 @@ import pathlib
 
 filePath = "file://" + str(pathlib.Path(__file__).parent.resolve())[:-5].replace("\\", "/")
 
+# Filsökvägen till rotmappen
 startPage = filePath + "index.html"  
 
 class test_Products(BaseCase):
+    #Kollar om produkterna finns på sidan
     def testProduct(self):
         self.open(startPage)
         for i in range(1, 7):
