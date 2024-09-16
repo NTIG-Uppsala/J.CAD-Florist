de274ec106e37805149bd1796c4825e56fb9045e
JCTe4 <jesper.cejie@elev.ga.ntig.se>
2024-09-16 10:55:33 +0200

    uppdaterat meddelande som visas när det är stängt pga helgdag samt lagt till en punkt efter 'kl'

--- tests/test_dynamicOpenHoursDays.py
+++ tests/test_dynamicOpenHoursDays.py
@@ -8,52 +8,52 @@ class TestDynamicOpenHoursDays(TestBase):
 
     # Tests that the open hours are displayed correctly during different times of Monday
     def testDynamicOpenHoursDisplayMonday(self) -> None:
-        self.setTimeAndAssertMatches(2024, 9, 9, 9, 59, matches=["Vi har stängt. Vi öppnar kl 10:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 9, 10, 0, matches=["Vi har öppet. Vi stänger kl 18:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 9, 17, 59, matches=["Vi har öppet. Vi stänger kl 18:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 9, 18, 0, matches=["Vi har stängt. Vi öppnar kl 10:00 på tisdag"])
+        self.setTimeAndAssertMatches(2024, 9, 9, 9, 59, matches=["Vi har stängt. Vi öppnar kl. 10:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 9, 10, 0, matches=["Vi har öppet. Vi stänger kl. 18:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 9, 17, 59, matches=["Vi har öppet. Vi stänger kl. 18:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 9, 18, 0, matches=["Vi har stängt. Vi öppnar kl. 10:00 på tisdag"])
 
     # Tests that the open hours are displayed correctly during different times of Tuesday
     def testDynamicOpenHoursDisplayTuesday(self) -> None:
-        self.setTimeAndAssertMatches(2024, 9, 10, 9, 59, matches=["Vi har stängt. Vi öppnar kl 10:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 10, 10, 0, matches=["Vi har öppet. Vi stänger kl 18:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 10, 17, 59, matches=["Vi har öppet. Vi stänger kl 18:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 10, 18, 0, matches=["Vi har stängt. Vi öppnar kl 10:00 på onsdag"])
+        self.setTimeAndAssertMatches(2024, 9, 10, 9, 59, matches=["Vi har stängt. Vi öppnar kl. 10:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 10, 10, 0, matches=["Vi har öppet. Vi stänger kl. 18:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 10, 17, 59, matches=["Vi har öppet. Vi stänger kl. 18:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 10, 18, 0, matches=["Vi har stängt. Vi öppnar kl. 10:00 på onsdag"])
 
     # Tests that the open hours are displayed correctly during different times of Wednesday
     def testDynamicOpenHoursDisplayWednesday(self) -> None:
-        self.setTimeAndAssertMatches(2024, 9, 11, 9, 59, matches=["Vi har stängt. Vi öppnar kl 10:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 11, 10, 0, matches=["Vi har öppet. Vi stänger kl 17:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 11, 16, 59, matches=["Vi har öppet. Vi stänger kl 17:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 11, 17, 0, matches=["Vi har stängt. Vi öppnar kl 10:00 på torsdag"])
+        self.setTimeAndAssertMatches(2024, 9, 11, 9, 59, matches=["Vi har stängt. Vi öppnar kl. 10:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 11, 10, 0, matches=["Vi har öppet. Vi stänger kl. 17:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 11, 16, 59, matches=["Vi har öppet. Vi stänger kl. 17:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 11, 17, 0, matches=["Vi har stängt. Vi öppnar kl. 10:00 på torsdag"])
 
     # Tests that the open hours are displayed correctly during different times of Thursday
     def testDynamicOpenHoursDisplayThursday(self) -> None:
-        self.setTimeAndAssertMatches(2024, 9, 12, 9, 59, matches=["Vi har stängt. Vi öppnar kl 10:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 12, 10, 0, matches=["Vi har öppet. Vi stänger kl 17:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 12, 16, 59, matches=["Vi har öppet. Vi stänger kl 17:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 12, 17, 0, matches=["Vi har stängt. Vi öppnar kl 10:00 på fredag"])
+        self.setTimeAndAssertMatches(2024, 9, 12, 9, 59, matches=["Vi har stängt. Vi öppnar kl. 10:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 12, 10, 0, matches=["Vi har öppet. Vi stänger kl. 17:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 12, 16, 59, matches=["Vi har öppet. Vi stänger kl. 17:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 12, 17, 0, matches=["Vi har stängt. Vi öppnar kl. 10:00 på fredag"])
 
     # Tests that the open hours are displayed correctly during different times of Friday
     def testDynamicOpenHoursDisplayFriday(self) -> None:
-        self.setTimeAndAssertMatches(2024, 9, 13, 9, 59, matches=["Vi har stängt. Vi öppnar kl 10:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 13, 10, 0, matches=["Vi har öppet. Vi stänger kl 18:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 13, 17, 59, matches=["Vi har öppet. Vi stänger kl 18:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 13, 18, 0, matches=["Vi har stängt. Vi öppnar kl 12:00 på lördag"])
+        self.setTimeAndAssertMatches(2024, 9, 13, 9, 59, matches=["Vi har stängt. Vi öppnar kl. 10:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 13, 10, 0, matches=["Vi har öppet. Vi stänger kl. 18:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 13, 17, 59, matches=["Vi har öppet. Vi stänger kl. 18:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 13, 18, 0, matches=["Vi har stängt. Vi öppnar kl. 12:00 på lördag"])
 
     # Tests that the open hours are displayed correctly during different times of Saturday
     def testDynamicOpenHoursDisplaySaturday(self) -> None:
-        self.setTimeAndAssertMatches(2024, 9, 14, 9, 59, matches=["Vi har stängt. Vi öppnar kl 12:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 14, 12, 0, matches=["Vi har öppet. Vi stänger kl 16:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 14, 15, 59, matches=["Vi har öppet. Vi stänger kl 16:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 14, 16, 0, matches=["Vi har stängt. Vi öppnar kl 12:00 på söndag"])
+        self.setTimeAndAssertMatches(2024, 9, 14, 9, 59, matches=["Vi har stängt. Vi öppnar kl. 12:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 14, 12, 0, matches=["Vi har öppet. Vi stänger kl. 16:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 14, 15, 59, matches=["Vi har öppet. Vi stänger kl. 16:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 14, 16, 0, matches=["Vi har stängt. Vi öppnar kl. 12:00 på söndag"])
 
     # Tests that the open hours are displayed correctly during different times of Sunday
     def testDynamicOpenHoursDisplaySunday(self) -> None:
-        self.setTimeAndAssertMatches(2024, 9, 15, 9, 59, matches=["Vi har stängt. Vi öppnar kl 12:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 15, 12, 0, matches=["Vi har öppet. Vi stänger kl 15:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 15, 14, 59, matches=["Vi har öppet. Vi stänger kl 15:00 idag"])
-        self.setTimeAndAssertMatches(2024, 9, 15, 15, 0, matches=["Vi har stängt. Vi öppnar kl 10:00 på måndag"])
+        self.setTimeAndAssertMatches(2024, 9, 15, 9, 59, matches=["Vi har stängt. Vi öppnar kl. 12:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 15, 12, 0, matches=["Vi har öppet. Vi stänger kl. 15:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 15, 14, 59, matches=["Vi har öppet. Vi stänger kl. 15:00 idag"])
+        self.setTimeAndAssertMatches(2024, 9, 15, 15, 0, matches=["Vi har stängt. Vi öppnar kl. 10:00 på måndag"])
 
 
 if __name__ == "__main__":
--- tests/test_dynamicOpenHoursHolidays.py
+++ tests/test_dynamicOpenHoursHolidays.py
@@ -8,43 +8,43 @@ class TestDynamicOpenHoursHolidays(TestBase):
 
     # Tests that the open hours are displayed on New Year's Day
     def testDynamicOpenHoursDisplayNewYearsDay(self) -> None:
-        self.setTimeAndAssertMatches(2024, 1, 1, 9, 59, matches=["Det är helgdag. Vi har stängt. Vi öppnar kl 10:00 på tisdag"])
-        self.setTimeAndAssertMatches(2024, 1, 2, 9, 59, matches=["Vi har stängt. Vi öppnar kl 10:00 idag"])
+        self.setTimeAndAssertMatches(2024, 1, 1, 9, 59, matches=["Idag har vi stängt p.g.a helgdag. Vi öppnar kl. 10:00 på tisdag"])
+        self.setTimeAndAssertMatches(2024, 1, 2, 9, 59, matches=["Vi har stängt. Vi öppnar kl. 10:00 idag"])
 
     # Tests that the open hours are displayed on Epiphany
     def testDynamicOpenHoursDisplayEpiphany(self) -> None:
-        self.setTimeAndAssertMatches(2024, 1, 6, 9, 59, matches=["Det är helgdag. Vi har stängt. Vi öppnar kl 12:00 på söndag"])
-        self.setTimeAndAssertMatches(2024, 1, 7, 11, 59, matches=["Vi har stängt. Vi öppnar kl 12:00 idag"])
+        self.setTimeAndAssertMatches(2024, 1, 6, 9, 59, matches=["Idag har vi stängt p.g.a helgdag. Vi öppnar kl. 12:00 på söndag"])
+        self.setTimeAndAssertMatches(2024, 1, 7, 11, 59, matches=["Vi har stängt. Vi öppnar kl. 12:00 idag"])
 
     # Tests that the open hours are displayed on May Day
     def testDynamicOpenHoursDisplayMayDay(self) -> None:
-        self.setTimeAndAssertMatches(2024, 5, 1, 9, 59, matches=["Det är helgdag. Vi har stängt. Vi öppnar kl 10:00 på torsdag"])
-        self.setTimeAndAssertMatches(2024, 5, 2, 9, 59, matches=["Vi har stängt. Vi öppnar kl 10:00 idag"])
+        self.setTimeAndAssertMatches(2024, 5, 1, 9, 59, matches=["Idag har vi stängt p.g.a helgdag. Vi öppnar kl. 10:00 på torsdag"])
+        self.setTimeAndAssertMatches(2024, 5, 2, 9, 59, matches=["Vi har stängt. Vi öppnar kl. 10:00 idag"])
 
     # Tests that the open hours are displayed on the National Day of Sweden
     def testDynamicOpenHoursDisplayNationalDayOfSweden(self) -> None:
-        self.setTimeAndAssertMatches(2024, 6, 6, 9, 59, matches=["Det är helgdag. Vi har stängt. Vi öppnar kl 10:00 på fredag"])
-        self.setTimeAndAssertMatches(2024, 6, 7, 9, 59, matches=["Vi har stängt. Vi öppnar kl 10:00 idag"])
+        self.setTimeAndAssertMatches(2024, 6, 6, 9, 59, matches=["Idag har vi stängt p.g.a helgdag. Vi öppnar kl. 10:00 på fredag"])
+        self.setTimeAndAssertMatches(2024, 6, 7, 9, 59, matches=["Vi har stängt. Vi öppnar kl. 10:00 idag"])
 
     # Tests that the open hours are displayed on Christmas Eve
     def testDynamicOpenHoursDisplayChristmasEve(self) -> None:
-        self.setTimeAndAssertMatches(2024, 12, 24, 9, 59, matches=["Det är helgdag. Vi har stängt. Vi öppnar kl 10:00 på fredag"])
-        self.setTimeAndAssertMatches(2024, 12, 25, 9, 59, matches=["Det är helgdag. Vi har stängt. Vi öppnar kl 10:00 på fredag"])
+        self.setTimeAndAssertMatches(2024, 12, 24, 9, 59, matches=["Idag har vi stängt p.g.a helgdag. Vi öppnar kl. 10:00 på fredag"])
+        self.setTimeAndAssertMatches(2024, 12, 25, 9, 59, matches=["Idag har vi stängt p.g.a helgdag. Vi öppnar kl. 10:00 på fredag"])
 
     # Tests that the open hours are displayed on Christmas Day
     def testDynamicOpenHoursDisplayChristmasDay(self) -> None:
-        self.setTimeAndAssertMatches(2024, 12, 25, 9, 59, matches=["Det är helgdag. Vi har stängt. Vi öppnar kl 10:00 på fredag"])
-        self.setTimeAndAssertMatches(2024, 12, 26, 9, 59, matches=["Det är helgdag. Vi har stängt. Vi öppnar kl 10:00 på fredag"])
+        self.setTimeAndAssertMatches(2024, 12, 25, 9, 59, matches=["Idag har vi stängt p.g.a helgdag. Vi öppnar kl. 10:00 på fredag"])
+        self.setTimeAndAssertMatches(2024, 12, 26, 9, 59, matches=["Idag har vi stängt p.g.a helgdag. Vi öppnar kl. 10:00 på fredag"])
 
     # Tests that the open hours are displayed on Boxing Day
     def testDynamicOpenHoursDisplayBoxingDay(self) -> None:
-        self.setTimeAndAssertMatches(2024, 12, 26, 9, 59, matches=["Det är helgdag. Vi har stängt. Vi öppnar kl 10:00 på fredag"])
-        self.setTimeAndAssertMatches(2024, 12, 27, 9, 59, matches=["Vi har stängt. Vi öppnar kl 10:00 idag"])
+        self.setTimeAndAssertMatches(2024, 12, 26, 9, 59, matches=["Idag har vi stängt p.g.a helgdag. Vi öppnar kl. 10:00 på fredag"])
+        self.setTimeAndAssertMatches(2024, 12, 27, 9, 59, matches=["Vi har stängt. Vi öppnar kl. 10:00 idag"])
 
     # Tests that the open hours are displayed on New Year's Eve
     def testDynamicOpenHoursDisplayNewYearsEve(self) -> None:
-        self.setTimeAndAssertMatches(2024, 12, 31, 9, 59, matches=["Det är helgdag. Vi har stängt. Vi öppnar kl 10:00 på torsdag"])
-        self.setTimeAndAssertMatches(2025, 1, 1, 9, 59, matches=["Det är helgdag. Vi har stängt. Vi öppnar kl 10:00 på torsdag"])
+        self.setTimeAndAssertMatches(2024, 12, 31, 9, 59, matches=["Idag har vi stängt p.g.a helgdag. Vi öppnar kl. 10:00 på torsdag"])
+        self.setTimeAndAssertMatches(2025, 1, 1, 9, 59, matches=["Idag har vi stängt p.g.a helgdag. Vi öppnar kl. 10:00 på torsdag"])
 
 
 if __name__ == "__main__":
