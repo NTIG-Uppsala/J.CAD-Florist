22f3d0f41b27ad26d479cf072524705e6e62cf3f
JCTe4 <jesper.cejie@elev.ga.ntig.se>
2024-09-18 14:20:09 +0200

    skrivit js-pseudokod för dagens klipp

---
+++ js/deal-of-the-day.js
@@ -0,0 +1,7 @@
+// const dealsOfTheDay = [ ... ]; all days that have deals, which products the deals apply to and what the deals are
+
+// Get the current day, date
+
+// Get the element of the product that is on sale today
+
+// set the deal of the day-elements of the product to be visible/active
