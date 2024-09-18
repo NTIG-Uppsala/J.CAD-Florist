cc38349eb96f1b7edf91e9448b749cc693c01f0e
JCTe4 <jesper.cejie@elev.ga.ntig.se>
2024-09-18 13:08:16 +0200

    förbättrat benämning av filnamn/mappnamn så att det blir tydligare samt säkerställer att screenshotmappen finns

--- tests/screenshots.py
+++ tests/screenshots.py
@@ -1,22 +1,23 @@
 from playwright.sync_api import sync_playwright
-from os import path
+from os import path, mkdir
 from datetime import datetime, timezone, timedelta
 import time
 
+screenshotFolder: str = "generated-screenshots" # folder for the screenshots
 
-def getFilePath(filePathFromRoot: str) -> str:
-    filePath = path.abspath(path.join(path.dirname(__file__), "..", filePathFromRoot))
-    return f"file://{filePath}"
+def getPagePath(pagePathFromRoot: str) -> str:
+    pagePath = path.abspath(path.join(path.dirname(__file__), "..", pagePathFromRoot))
+    return f"file://{pagePath}"
     
 resolutions : dict[str, dict[str, int]] = {
     "1080p": {"width": 1920, "height": 1080},
-    "1440p": {"width": 2560, "height": 1440},
-    "iPhone SE": {"width": 320, "height": 568},
-    "iPhone XR": {"width": 414, "height": 896},
-    "iPhone 12 Pro": {"width": 390, "height": 844},
-    "iPhone 14 Pro Max": {"width": 428, "height": 926},
-    "Pixel 7": {"width": 393, "height": 851},
-    "Samsung Galaxy S8+": {"width": 360, "height": 740},
+    # "1440p": {"width": 2560, "height": 1440},
+    # "iPhone SE": {"width": 320, "height": 568},
+    # "iPhone XR": {"width": 414, "height": 896},
+    # "iPhone 12 Pro": {"width": 390, "height": 844},
+    # "iPhone 14 Pro Max": {"width": 428, "height": 926},
+    # "Pixel 7": {"width": 393, "height": 851},
+    # "Samsung Galaxy S8+": {"width": 360, "height": 740},
     "Galaxy Fold": {"width": 280, "height": 653},
     "iPad": {"width": 768, "height": 1024},
 }
@@ -29,12 +30,12 @@ locators : dict[str, dict[str, str | None]] = {
     "footer": {"selector": "footer", "button": None},
 }
 
-def takeScreenshot(filePath : str, outputFile : str, resolution : dict[str, dict[str, int]], locator : dict[str, dict[str, str | None]]) -> None:
+def takeScreenshot(pagePath : str, outputFile : str, resolution : dict[str, dict[str, int]], locator : dict[str, dict[str, str | None]]) -> None:
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
 
-        fileUrl = getFilePath(filePath)
+        fileUrl = getPagePath(pagePath)
         page.set_viewport_size(resolution)
         page.goto(fileUrl)
 
@@ -63,11 +64,16 @@ def getCurrentDateAndTime(): # function for getting the current date and time
     return formattedTime # return the current date and time in a specific format
 
 def genFileName(resIndex : int, locIndex : int, res : str, loc : str, currentDateAndTime : str) -> str: # function for generating the filename aswell as a folder for the current batch of screenshots
-    return f"{currentDateAndTime}/{resIndex}-{locIndex}-{res}-{loc}-{currentDateAndTime}.png"
+    return f"{resIndex}-{locIndex}-{res}-{loc}-{currentDateAndTime}.png"
 
 if __name__ == "__main__":
     dateAndTime = getCurrentDateAndTime() # get the current date and time for file naming
+
+    if not path.isdir(screenshotFolder): # create a folder for the screenshots if it doesn't exist
+        mkdir(screenshotFolder)
+
     for resIndex, res in enumerate(resolutions):
         for locIndex, loc in enumerate(locators):
+            folderName = dateAndTime
             fileName = genFileName(resIndex, locIndex, res, loc, dateAndTime)
-            takeScreenshot("index.html", f"generated-screenshots/{fileName}", resolutions[res], locators[loc])
+            takeScreenshot("index.html", f"{screenshotFolder}/{folderName}/{fileName}", resolutions[res], locators[loc])
