4cbbbc96189e5d6b447f083af8e7758dbbd625ed
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2024-09-19 09:09:35 +0200

    Lade till tester för de nya produkterna

--- tests/test_products.py
+++ tests/test_products.py
@@ -14,24 +14,36 @@ class TestProducts(TestBase):
         self.assertInText("Sommarbukett")
         self.assertInText("Höstbukett")
         self.assertInText("Begravningskrans")
+        self.assertInText("Liljor")
+        self.assertInText("Hortensia")
+        self.assertInText("Aloe vera")
+        self.assertInText("Kaktus i kruka")
 
     # Test that the product prices are displayed
-    def testPrices(self) -> None:
+    def testProductPrices(self) -> None:
         self.assertInText("1 200 kr")
         self.assertInText("150 kr")
         self.assertInText("100 kr")
         self.assertInText("200 kr")
         self.assertInText("400 kr")
         self.assertInText("800 kr")
+        self.assertInText("29 kr/st")
+        self.assertInText("59 kr")
+        self.assertInText("99 kr")
+        self.assertInText("99 kr")
 
     # Test that the product images are displayed
-    def testImages(self) -> None:
+    def testProductImages(self) -> None:
         self.assertInHTML('<img src="images/brollopsbukett.jpg" alt="bröllopsbukett">')
         self.assertInHTML('<img src="images/rosor-10-pack.jpg" alt="rosor 10-pack">')
         self.assertInHTML('<img src="images/tulpaner-10-pack.jpg" alt="tulpaner 10-pack">')
         self.assertInHTML('<img src="images/sommarbukett.jpg" alt="sommarbukett">')
         self.assertInHTML('<img src="images/hostbukett.jpg" alt="höstbukett">')
         self.assertInHTML('<img src="images/begravningskrans.jpg" alt="begravningskrans">')
+        self.assertInHTML('<img src="images/liljor.jpg" alt="liljor">')
+        self.assertInHTML('<img src="images/hortensia.jpg" alt="hortensia">')
+        self.assertInHTML('<img src="images/aloe-vera.jpg" alt="aloe vera">')
+        self.assertInHTML('<img src="images/kaktus-i-kruka.jpg" alt="kaktus i kruka">')
 
 
 if __name__ == "__main__":
