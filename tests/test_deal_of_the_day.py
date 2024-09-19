52bf8500d2724ebe790d735e6ca01ac9bff96b64
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2024-09-19 09:39:06 +0200

    Skrev tester för dagens klipp

--- tests/test_deal_of_the_day.py
+++ tests/test_deal_of_the_day.py
@@ -6,12 +6,30 @@ class TestDealOfTheDay(TestBase):
     def setUp(self) -> None:
         super().setUp(filePathFromRoot="index.html")
 
-    # define an array of objects that represent which product is the deal of the day for each day that the deal of the day is relevant
+    # Test that the deal of the day is displayed on Monday
+    def testDealsMonday(self) -> None:
+        self.setTimeAndAssertMatches(2024, 9, 16, 12, 0, matches=["Tulpaner 10-pack", "89 kr"])
 
-    # find the visible element with the deal of the day
-    # check that it is accurate relative to the current date and that the price is correct
-        # if current day is x then the deal of the day should be visible on product X
-        # if current day is y then the deal of the day should be visible on product Y
+    # Test that the deal of the day is displayed on Tuesday
+    def testDealsTuesday(self) -> None:
+        self.setTimeAndAssertMatches(2024, 9, 17, 12, 0, matches=["Liljor", "19 kr/st"])
+
+    # Test that the deal of the day is displayed on Wednesday
+    def testDealsWednesday(self) -> None:
+        self.setTimeAndAssertMatches(2024, 9, 18, 12, 0, matches=["Hortensia", "39 kr"])
+
+    # Test that the deal of the day is displayed on Thursday
+    def testDealsThursday(self) -> None:
+        self.setTimeAndAssertMatches(2024, 9, 19, 12, 0, matches=["Aloe vera", "79 kr"])
+
+    # Test that the deal of the day is displayed on Friday
+    def testDealsFriday(self) -> None:
+        self.setTimeAndAssertMatches(2024, 9, 20, 12, 0, matches=["Kaktus i kruka", "79 kr"])
+
+    # Test that the deal of the day is displayed on Saturday
+    def testDealsSaturday(self) -> None:
+        self.setTimeAndAssertMatches(2024, 9, 21, 12, 0, matches=["Rosor 10-pack", "127 kr"])
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
