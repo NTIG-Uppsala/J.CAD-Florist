b6ce076da8c207c8b3e9b7e483dde031e9b34584
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2024-09-23 11:26:19 +0200

    Fixade ctrl + zoom samt förbättrade mobilanpassning

--- js/embed-osm-map.js
+++ js/embed-osm-map.js
@@ -1,14 +1,11 @@
 // Position of the marker and center of the map
 const position = [67.86593, 20.2341];
 
-// Check if user is on a computer or mobile device
-const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-
 // Map options including center and zoom level
 let mapOptions = {
     center: position,
     zoom: 16,
-    dragging: !isMobile,
+    gestureHandling: true,
 };
 
 // Create the map
@@ -21,16 +18,3 @@ map.addLayer(layer);
 // Add a marker
 let marker = new L.Marker(position);
 marker.addTo(map);
-
-const mapEl = document.querySelector("#map");
-
-mapEl.addEventListener("touchstart", onTwoFingerDrag);
-mapEl.addEventListener("touchend", onTwoFingerDrag);
-
-const onTwoFingerDrag = (event) => {
-    if (event.touches.length === 2) {
-        map.dragging.enable();
-    } else {
-        map.dragging.disable();
-    }
-};
