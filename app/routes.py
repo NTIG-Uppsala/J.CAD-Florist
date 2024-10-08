dac7c2723cebe1d819e831836afa441b1ba99408
ivar117 <ivar.bjerling@elev.ga.ntig.se>
2024-10-08 15:58:39 +0200

    sqlalchemy sqlite database configuration, database models
    updated requirements.txt, database migrations

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,16 @@ from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
 @app.route('/se')
-def index():
+def se():
+    return render_template('index.html')
+
+@app.route('/en')
+def en():
+    return render_template('index.html')
+
+@app.route('/no')
+def no():
     return render_template('index.html')
 
 # Istället för handlebars har en index fil som en template som ändrar språk beroende på vilken route som används
