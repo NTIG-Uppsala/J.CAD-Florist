327c20049b9d4949f50227f56029abebac46094b
ivar117 <ivar.bjerling@elev.ga.ntig.se>
2024-10-11 15:58:21 +0200

    added proof of concept database model, created table from CompanyInformation model

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,13 @@
 from flask import render_template
 from app import app
 
-# Istället för handlebars har en index fil som en template som ändrar språk beroende på vilken route som används
-# SKicka data vid route så att det kan användas av jinja i templates
-
 @app.route('/')
 @app.route('/se')
 def se():
-    return render_template('index.html')
+    lang = "sv"
+    return render_template('index.html', lang=lang)
 
-# @app.route('/en')
-# def en():
-#     return render_template('index.html')
-
-# @app.route('/no')
-# def no():
-#     return render_template('index.html')
-
-# @app.route('/admin')
-# def admin():
-#     return render_template('admin.html')
+@app.route('/admin')
+def admin():
+    lang = "sv"
+    return render_template('admin.html', lang=lang)
--- florista.py
+++ florista.py
@@ -1,4 +1,9 @@
-from app import app
+from app import app, db
+from app.models import CompanyInformation
 
 if __name__ == '__main__':
     app.run(debug=False)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'CompanyInformation': CompanyInformation}
