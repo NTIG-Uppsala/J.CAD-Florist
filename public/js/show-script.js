bd93ec10fa0d86ca1bcbbb99ebbeacb5cef4c60b
ivar117 <ivar.bjerling@elev.ga.ntig.se>
2024-10-14 15:04:45 +0200

    cleanup of old code
    removed the 'public/' dir and various files for the old project structure before flask

--- public/js/show-script.js
+++
@@ -1,5 +0,0 @@
-// Loops through all elements with the class "script" and sets their display to block
-// The class is used on elements that are not functional without JavaScript enabled
-document.querySelectorAll(".script").forEach((script) => {
-    script.style = "display: block;";
-});
--- public/js/testing-wait-for-js.js
+++
@@ -1,6 +0,0 @@
-// Adds a hidden div to the body element with the id JSLoaded to indicate that the JavaScript has loaded. This is used in the testing to ensure that all JavaScript has executed before running the tests.
-
-const JSLoaded = document.createElement("div");
-JSLoaded.setAttribute("hidden", "true");
-JSLoaded.setAttribute("id", "JSLoaded");
-document.body.appendChild(JSLoaded);
